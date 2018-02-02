518b51b3b6807b85e2daafdb012c5aef7b062c34
linazoo <linazooper@gmail.com>
2018-02-01 22:20:13 -0600

    created http server so we could listen to the request

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 // main starting point of the application
 
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+//APP setup
+// all about express set up
+
+//server setup
+
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on:', port);
