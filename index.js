16da4a69d48140f9729bb3cd8673bce6c67c850f
linazoo <linazooper@gmail.com>
2018-02-01 23:18:38 -0600

    added a route handler for the / route sending back a response successfully

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const router = require ('./router');
 
 //APP setup
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*'}));
-
+router(app);
 // all about express set up
 
 //server setup
