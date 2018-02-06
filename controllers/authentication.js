bed00cc74e48a365c05c888da7f26af5c15160da
linazoo <linazooper@gmail.com>
2018-02-05 22:17:04 -0600

    setting up a way to find if user exists with findOne

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,22 @@
+const User = require('../models/user');
+
+
+
 exports.signup = function(req, res, next) {
-  res.send({ success: 'true' });
+  const email = req.body.email;
+  const password = req.body.password;
+  
+  // see if a user with a given email exists because we dont want duplicates
+
+  User.findOne({ email: email }, function(err, existingUser) {
+
+  });
+  // if they do exist return error
+  
+
+  // if they do not exist , create and save user record
+
+  // respond to request indicating the user was created 
+
+  
 }
